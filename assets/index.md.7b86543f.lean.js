17a3c94fa577f59275f51f399e3d65a2e6cb516b
maximillian <makcmakc321@mail.ru>
2022-01-16 22:17:32 +0300

    deploy

---
+++ assets/index.md.7b86543f.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o as a,a as t}from"./app.9d73b456.js";const m='{"title":"Hello RevPush","description":"","frontmatter":{},"headers":[{"level":3,"title":"Routes","slug":"routes"}],"relativePath":"index.md","lastUpdated":1642360647103}',o={},n=t("",4),c=[n];function l(r,d,i,p,u,_){return a(),s("div",null,c)}var v=e(o,[["render",l]]);export{m as __pageData,v as default};
